2f2052c6404682b9958d70bb2cca56746c47a48c
göksel gürlük <gurlukgoksel@gmail.com>
2025-04-27 19:40:54 +0300

    all pages complated

--- getir-clone/frontend/src/ui/CategoryItem.jsx
+++ getir-clone/frontend/src/ui/CategoryItem.jsx
@@ -2,7 +2,7 @@ function CategoryItem({ category }) {
 	return (
         <a href="#" className="flex group justify-center items-center rounded transition-colors hover:bg-purple-50 flex-col p-4">
             <img src={category.image} className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
-            <span className="font-medium opacity-70 transition-colors group-hover:text-secondary whitespace-nowrap block mt-2 text-sm">{category.title}</span>
+            <span className="font-semibold opacity-70 transition-colors group-hover:text-secondary whitespace-nowrap block mt-2 text-sm">{category.title}</span>
         </a>
 	)
 }
